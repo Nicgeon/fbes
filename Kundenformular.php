ae548508637d7f7b2c754f846eaf7df5fef7975b
Nicgeon <phlppmoser@gmail.com>
2025-01-18 16:51:51 +0100

    Mainly just design changes

--- Kundenformular.php
+++ Kundenformular.php
@@ -7,35 +7,42 @@
     <title>Document</title>
 </head>
 <body>
-    <center><h1>Wo möchten sie hin?</h1></center><br><br><br>
-    <h3>Bitte Wählen sie Ihre <b>Anfangsstaion</b> sowie ihre <b>Endstation</b> aus.</h3><br>
+    <div class="content">
+        <center><h1>Wo möchten sie hin?</h1></center><br>
+        <h3>Bitte Wählen sie Ihre <b>Anfangsstaion</b> sowie ihre <b>Endstation</b> aus.</h3>
 
-    <form action="./eintrag.php" method="get">
-        <select id="Stationen" name="von" >
-            <?php
-                $PDO = new PDO('mysql:host=localhost; dbname=fbes;charset=utf8', 'root', '');
-                $sql = "SELECT stationen.NAME
-                        FROM stationen";
-                
-                foreach($PDO->query($sql) as $row){
-                    echo "<option value='".$row['NAME']."'>".$row['NAME']."</option>";
-                }
-            ?>
-        </select>
-        bis
-        <select id="Stationen" name="bis">
-            <?php
-                $PDO = new PDO('mysql:host=localhost; dbname=fbes;charset=utf8', 'root', '');
-                $sql = "SELECT stationen.NAME
-                        FROM stationen";
-                
-                foreach($PDO->query($sql) as $row){
-                    echo "<option value='".$row['NAME']."'>".$row['NAME']."</option>";
-                }
-            ?>
-        </select>
-        <br><br><br>
-        <input type="submit" name="submit" size="5">
-    </from>
+        <form action="./eintrag.php" method="get">
+            <select id="Stationen" name="von" >
+                <?php
+                    $PDO = new PDO('mysql:host=localhost; dbname=fbes;charset=utf8', 'root', '');
+                    $sql = "SELECT stationen.NAME
+                            FROM stationen";
+                    
+                    foreach($PDO->query($sql) as $row){
+                        echo "<option value='".$row['NAME']."'>".$row['NAME']."</option>";
+                    }
+                ?>
+            </select>
+            bis
+            <select id="Stationen" name="bis">
+                <?php
+                    $PDO = new PDO('mysql:host=localhost; dbname=fbes;charset=utf8', 'root', '');
+                    $sql = "SELECT stationen.NAME
+                            FROM stationen";
+                    
+                    foreach($PDO->query($sql) as $row){
+                        echo "<option value='".$row['NAME']."'>".$row['NAME']."</option>";
+                    }
+                ?>
+            </select>
+            <br><br><br>
+            <input type="submit" name="submit" size="5">
+        </from>
+    </div>
+    <footer class="footer">
+        <form action="./Login.php" method="post" class="login-form">
+            <input type="submit" value="Busfahrer Login" class="login-button">
+        </form>
+    </footer>
 </body>
 </html>
