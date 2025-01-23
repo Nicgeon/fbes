157bd6006d62c32f1f166c688b305cad0d22702f
Philipp Moser <phlppmoser@gmail.com>
2025-01-23 09:37:31 +0100

    hauiwghdagidga8iwg7f7zgaw no comment

--- Anzeige.php
+++ Anzeige.php
@@ -9,7 +9,7 @@
 <body>
     <center><h1>Fahrtanzeige</h1></center>
 
-    <table>
+    <table id="myTable">
     <?php 
         session_start();
         if(!isset($_SESSION['Login']) || !$_SESSION['Login'] == true) {
@@ -18,7 +18,7 @@
         }
         $Linie = $_SESSION['Linie'];
 
-        $PDO = new PDO('mysql:host=localhost; dbname=fbes;charset=utf8', 'root', '');
+        $PDO = new PDO('mysql:host=localhost; dbname=fbes;charset=utf8', 'fbes', '1234');
 
         $sql = "SELECT stationen.Name, verbindungen.Wartet, verbindungen.Uhrzeit
                 FROM linie 
@@ -32,10 +32,36 @@
         $stmt = $PDO->prepare($sql);
 
         foreach($PDO->query($sql) as $row) {
-            echo "<tr><td>".$row['Name']."<br>".$row['Uhrzeit']."</td><td>".$row['Wartet']."</td></tr>";
+            echo "<tr><td><b>".$row['Name']."</b><br>".$row['Uhrzeit']."</td><td>".$row['Wartet']."</td></tr>";
         }
-
     ?>
     </table>
+
+    <script>
+            const table = document.getElementById('myTable');
+            let currentHighlight = 0;
+
+            window.addEventListener('wheel', (event) => {
+            const rows = table.getElementsByTagName('tr');
+            
+            // Entferne die Hervorhebung von der aktuellen Zeile
+            if (rows[currentHighlight]) {
+                rows[currentHighlight].classList.remove('highlight');
+            }
+            
+            // Ändere den Index basierend auf der Scrollrichtung
+            if (event.deltaY > 0) {
+                currentHighlight = (currentHighlight + 1) % rows.length;
+            } else {
+                currentHighlight = (currentHighlight - 1 + rows.length) % rows.length;
+            }
+            
+            // Füge die Hervorhebung zur neuen Zeile hinzu
+            rows[currentHighlight].classList.add('highlight');
+            
+            // Verhindere das normale Scrollverhalten
+            event.preventDefault();
+            });
+    </script>
 </body>
 </html>
